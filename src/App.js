ce2ce3773ed837e9de364ed375f62d45c34c48ad
tryHonestly <lordikus02021999@gmail.com>
2022-07-05 14:24:47 +0300

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Post from './components/Post';
 
 function App() {
+
+  const [posts, setPosts] = useState([])
+
+  const fetchPosts = async () => {
+    const asyncRes = await axios.get(`https://jsonplaceholder.typicode.com/comments`,{
+      params:{
+        _limit:20
+      }
+    })
+    setPosts(asyncRes.data)
+  }
+
+  
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   return (
     <div className="App">
-      
+      {posts.map(p => <Post post={p} key={p.id}/>)}
     </div>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const Post = (post) => {
+
+ 
+  return (
+    <div style={{
+      border: `solid 2px black`,
+      display:`flex`,
+      flexDirection:`column`,
+      gap:`5px`
+
+    }}>
+      <p>іфвфі</p>
+      <p>іфвфі</p>
+      <p>іфвфі</p>
+      <p>іфвфі</p>
+
+    </div>
+  )
+}
+
+export default Post
