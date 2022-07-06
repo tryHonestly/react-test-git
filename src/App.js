45b83155b8f1b895d5d90694049f9d2fd12aa963
tryHonestly <lordikus02021999@gmail.com>
2022-07-06 12:36:20 +0300

    added ADD POST function

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Post from './components/Post';
 
+
+
+
+
+
 function App() {
 
   const [posts, setPosts] = useState([])
+  const [postValue, setPostValue] = useState(``)
 
   const fetchPosts = async () => {
     const asyncRes = await axios.get(`https://jsonplaceholder.typicode.com/comments`,{
@@ -21,8 +27,25 @@ function App() {
     fetchPosts()
   }, [])
 
+  console.log(postValue);
+  const clickHandler = () => {
+    const newPost = {
+      body: postValue,
+      email: "TEST@TEST",
+      id: 17,
+      name: "TEST",
+      postId: 4,
+    }
+    setPosts([...posts, newPost])
+  }
   return (
     <div className="App">
+      <div>
+        <h1>add post</h1>
+        <input type="text" value={postValue} onChange={(e) => {setPostValue(e.target.value)}}/>
+        <button onClick={clickHandler}>add</button>
+      </div>
+
       {posts.map(p => <Post post={p} key={p.id}/>)}
     </div>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Post = ({post}) => {
 
-  console.log(post);
+  
  
   return (
     <div style={{
